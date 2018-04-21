c202274ec624be488197d93948e82197231ccc2f
Kensuke Kousaka <k@k3n.link>
2018-04-21 18:15:37 +0900

    Implement sample button click event

--- app/src/main/kotlin/link/k3n/retrofitsampleapp/activities/MainActivity.kt
+++ app/src/main/kotlin/link/k3n/retrofitsampleapp/activities/MainActivity.kt
@@ -3,8 +3,10 @@ package link.k3n.retrofitsampleapp.activities
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import link.k3n.retrofitsampleapp.R
 import link.k3n.retrofitsampleapp.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
+        binding.button.setOnClickListener(this::runProcess)
+    }
+
+    private fun runProcess(v: View) {
+        Timber.d("Button clicked!")
     }
 }
